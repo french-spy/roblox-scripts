f9f9f076be48be548dc54731e061e05d6d54af39
french-spy <50294457+french-spy@users.noreply.github.com>
2021-05-25 15:23:07 +0300

    Create unConv anti afk.lua

---
+++ unConv anti afk.lua
@@ -0,0 +1,20 @@
+local LP = game:GetService("Players").LocalPlayer;
+local UnAFK = game:GetService("ReplicatedStorage").UnAFK;
+
+for _, v in pairs(getconnections(LP.Idled)) do
+    v:Disable(); 
+end
+
+LP:WaitForChild("PlayerScripts"):WaitForChild("gaming"):Destroy();
+
+while wait() do
+    if LP.PlayerGui.ScreenGui.AntiAFK.Visible then
+        UnAFK:FireServer(10);
+        LP.PlayerGui.ScreenGui.AntiAFK.Visible = false;
+    elseif LP.PlayerGui.ScreenGui.income.TextButton.Visible then
+        LP.PlayerGui.ScreenGui.income.TextButton.Visible = false;
+        LP.PlayerGui.ScreenGui.AntiAFK.Visible = true;
+        UnAFK:FireServer(10);
+        LP.PlayerGui.ScreenGui.AntiAFK.Visible = false;
+    end
+end
