6f448ff78975b814336eb0a2a9270125d0a28542
french-spy <50294457+french-spy@users.noreply.github.com>
2021-07-06 19:33:29 +0300

    Update auto getservice.lua

--- auto getservice.lua
+++ auto getservice.lua
@@ -4,7 +4,7 @@ setmetatable(services,
     __index = function(t, k)
         if not rawget(t, k) then
             rawset(t, k, game:GetService(k));
-            return rawget(t, k);
+            return t[k];
         end
     end;
 });
