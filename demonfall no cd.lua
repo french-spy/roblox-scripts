c1f58079f754dbf17f7aed9c60d84a0ec03355a0
french-spy <50294457+french-spy@users.noreply.github.com>
2021-07-13 20:15:04 +0300

    Update demonfall no cd.lua

--- demonfall no cd.lua
+++ demonfall no cd.lua
@@ -4,7 +4,7 @@ pcall(function()
     while wait() do
         if _G.noCD then
             for i,v in pairs(lp.Character:GetChildren()) do
-                if v:IsA("IntValue") and string.match(v.Name, "Cooldown") then
+                if v:IsA("IntValue") and string.match(v.Name, "Cooldown") or string.match(v.Name, "Busy") then
                     v:Destroy(); 
                 end
             end
