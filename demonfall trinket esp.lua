9e910f5e6915f464f1bb6a49e8161bee0aeacc1c
french-spy <50294457+french-spy@users.noreply.github.com>
2021-07-14 14:58:42 +0300

    Update demonfall trinket esp.lua

--- demonfall trinket esp.lua
+++ demonfall trinket esp.lua
@@ -1,46 +1,72 @@
-local lp = game:service"Players".LocalPlayer;
-local camera = game:service"Workspace".CurrentCamera;
+_G.trinketEsp = true
 
-local function azis(ledena_kralica)
-    local sentrope = Drawing.new("Text");
-    sentrope.Visible = false;
-    sentrope.Center = true;
-    sentrope.Outline = true;
-    sentrope.Font = 1;
-    sentrope.Size = 15;
-    sentrope.Text = ledena_kralica.Name;
-    sentrope.Color = Color3.fromRGB(0, 0, 0);
+local lp = game:service"Players".LocalPlayer;
+local ws = game:service"Workspace";
+local rs = game:service"RunService";
+local camera = ws.CurrentCamera;
+
+local function makeEsp(v)
+    local Name = Drawing.new("Text");
+    Name.Visible = false;
+    Name.Center = true;
+    Name.Outline = false;
+    Name.Font = 1;
+    Name.Size = 20;
+    Name.Color = Color3.fromRGB(255, 255, 255);
     
-    local function toni_storaro()
-        local c;
-        c = game:service"RunService".RenderStepped:Connect(function()
-            if ledena_kralica and game:service"Workspace":FindFirstChild(ledena_kralica.Name) and ledena_kralica:FindFirstChildWhichIsA("MeshPart") then
-                local vector, onScreen = camera:WorldToViewportPoint(ledena_kralica:FindFirstChildWhichIsA("MeshPart").Position);
+    local Distance = Drawing.new("Text");
+    Distance.Visible = false;
+    Distance.Center = true;
+    Distance.Outline = false;
+    Distance.Font = 1;
+    Distance.Size = 18;
+    Distance.Color = Color3.fromRGB(255, 255, 255);
+    
+    local function update()
+        local a;
+        a = rs.RenderStepped:Connect(function()
+            if v and v:IsDescendantOf(game) and v:FindFirstChild("Configuration") and v.Configuration:IsA("Configuration") and v:FindFirstChild("PickableItem") and v:FindFirstChildWhichIsA("MeshPart") then
+                local a;
+                if v:FindFirstChild("MeshPart") then
+                    a = v:FindFirstChild("MeshPart");
+                elseif v:FindFirstChild("Main") then
+                    a = v:FindFirstChild("Main");
+                elseif v:FindFirstChild("Main") and v:FindFirstChild("Part") then
+                    a = v:FindFirstChild("Part"); 
+                end
                 
-                if onScreen then
-                    sentrope.Position = Vector2.new(vector.X, vector.Y);
-                    sentrope.Visible = true;
-                else
-                    sentrope.Visible = false; 
+                local vector, onScreen = camera:WorldToViewportPoint(a.Position);
+                if _G.trinketEsp and onScreen then
+                    Name.Position = Vector2.new(vector.X, vector.Y);
+                    Name.Visible = true;
+                    Name.Text = v.Name;
+                    
+                    Distance.Position = Vector2.new(vector.X, vector.Y - 10);
+                    Distance.Visible = true;
+                    Distance.Text = tostring(math.floor((a.Position - lp.Character.HumanoidRootPart.Position).magnitude + 0.5)) .. " studs";
+                elseif not onScreen or not _G.trinketEsp then
+                    Name.Visible = false;
+                    Distance.Visible = false;
                 end
-            else
-                if not game:service"Workspace":FindFirstChild(ledena_kralica.Name) or not ledena_kralica:FindFirstChildWhichIsA("MeshPart") or not ledena_kralica:FindFirstChildWhichIsA("Part") then
-                    c:Disconnect(); 
-                end
-                sentrope.Visible = false;
+            elseif not v or not v:IsDescendantOf(game) or not v:FindFirstChildWhichIsA("MeshPart") then
+                Name:Remove()
+                Distance:Remove();
+                a:Disconnect();
             end
         end)
     end
-    coroutine.wrap(toni_storaro)();
+    update();
 end
-for i,v in next, game:service"Workspace":GetChildren() do
-    if v:IsA("Model") and v:FindFirstChild("Configuration") and v.Configuration:IsA("Configuration") and v:FindFirstChild("PickableItem") and v:FindFirstChildWhichIsA("MeshPart") then
-        azis(v); 
+
+for i,v in pairs(ws:GetChildren()) do
+    if v and v:IsDescendantOf(game) and v:FindFirstChild("Configuration") and v.Configuration:IsA("Configuration") and v:FindFirstChild("PickableItem") and v:FindFirstChildWhichIsA("MeshPart") then
+        pcall(makeEsp, v);
     end
 end
 
-game:service"Workspace".ChildAdded:Connect(function(c)
-    if c:IsA("Model") and c:FindFirstChild("Configuration") and c.Configuration:IsA("Configuration") and c:FindFirstChild("PickableItem") and c:FindFirstChildWhichIsA("MeshPart") then
-        azis(c); 
+ws.ChildAdded:Connect(function(v)
+    if v and v:IsDescendantOf(game) and v:FindFirstChild("Configuration") and v.Configuration:IsA("Configuration") and v:FindFirstChild("PickableItem") and v:FindFirstChildWhichIsA("MeshPart") then
+        --print(v.Name .. " spawned");
+        pcall(makeEsp, v);    
     end
 end)
