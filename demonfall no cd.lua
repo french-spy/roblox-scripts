f5db65501038b8da3802fcd815faf6d7001e7bf4
french-spy <50294457+french-spy@users.noreply.github.com>
2021-07-16 09:05:10 +0300

    Update demonfall no cd.lua

--- demonfall no cd.lua
+++ demonfall no cd.lua
@@ -1,13 +1,27 @@
 local lp = game:service"Players".LocalPlayer;
 _G.noCD = true
-pcall(function()
-    while wait() do
-        if _G.noCD then
+local a = coroutine.wrap(function()
+    pcall(function()
+        while _G.noCD do wait()
             for i,v in pairs(lp.Character:GetChildren()) do
-                if v:IsA("IntValue") and string.match(v.Name, "Cooldown") or string.match(v.Name, "Busy") then
+                if v:IsA("IntValue") and string.match(v.Name, "Cooldown") or string.match(v.Name,"Busy") then
                     v:Destroy(); 
                 end
             end
         end
-    end
+    end)
 end)
+a();
+
+local b = coroutine.wrap(function()
+    pcall(function()
+        while _G.noCD do wait()
+            for i,v in pairs(lp:GetChildren()) do
+                if string.match(v.Name, "Cooldown") or string.match(v.Name, "Busy") then
+                    v:Destroy(); 
+                end
+            end
+        end
+    end) 
+end)
+b();
