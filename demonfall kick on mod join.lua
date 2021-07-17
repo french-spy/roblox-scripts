928e60d962b85a6dc8dc1b6dfe84027e83f019ba
french-spy <50294457+french-spy@users.noreply.github.com>
2021-07-17 10:20:38 +0300

    Update demonfall kick on mod join.lua

--- demonfall kick on mod join.lua
+++ demonfall kick on mod join.lua
@@ -1,38 +1,14 @@
-local modsIds = 
-{
-	666087238,
-	927875377,
-	1337156246,
-	742406104,
-	1483657440,
-	1224956693,
-	477198208,
-	150657944,
-	60676205,
-	1188893926,
-	879624436,
-	85778076,
-	472236361,
-	141687683,
-	2271659462,
-	399930437,
-	382649287,
-	548334040,
-	1587973624,
-	276017027,
-	304646590,
-	56969615,
-	28392137
-}
+local groupId = 4294803
+local roles = {"Moderators", "Admins", "Developers", "Torso", "Head"};
 
 for i,v in pairs(game:service"Players":GetPlayers()) do
-	if modsIds[v.UserId] then
+	if roles[v:GetRoleInGroup(groupId)] then
 		game:service"Players".LocalPlayer:Kick("Mod is in the server");
 	end
 end
 
 game:service"Players".PlayerAdded:Connect(function(p)
-	if modsIds[p.UserId] then
+	if roles[p:GetRoleInGroup(groupId)] then
 		game:service"Players".LocalPlayer:Kick("Mod joined the server");
 	end
 end)
