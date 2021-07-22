eb5bc7d43f644e95e27d25a602922bddae4ab129
french-spy <50294457+french-spy@users.noreply.github.com>
2021-07-22 10:24:57 +0300

    Update demonfall mob autofarm insta tp.lua

--- demonfall mob autofarm insta tp.lua
+++ demonfall mob autofarm insta tp.lua
@@ -2,6 +2,7 @@ _G.mob = "Zenitsu"; --GenericOni/GenericSlayer/Green Demon(?)
 _G.noclip = true;
 _G.speed = 45;
 _G.distFromMob = 6;
+_G.maxDist = math.huge;
 _G.autofarm = true;
 _G.notExecuted = true; --dont touch
 
@@ -14,14 +15,12 @@ local ws = game:service"Workspace";
 local runs = game:service"RunService";
 local vim = game:service"VirtualInputManager"; 
 
-local tpBypass = loadstring(game:HttpGet("https://raw.githubusercontent.com/french-spy/roblox-scripts/main/demonfall%20tp%20bypass.lua"))();
-local drops = {"Green Horn", "Demon Horn", "Broken Nichirin", "Crystal Essence", "Demon Collar"};
-
 local spawns = 
 {
 	["Zenitsu"] = CFrame.new(-2537.05103, 941.436035, -3351.02222, 0.30188486, -1.47914374e-08, 0.953344405, 5.87038906e-09, 1, 1.36564031e-08, -0.953344405, 1.47384116e-09, 0.30188486),
 	["Green Demon"] = CFrame.new(1474.98828, 818.827637, -6379.07861, 0.00430911547, -7.22375617e-08, -0.999990702, -4.87611951e-10, 1, -7.22403328e-08, 0.999990702, 7.98899336e-10, 0.00430911547)
 }
+local drops = {"Green Horn", "Demon Horn", "Broken Nichirin", "Crystal Essence", "Demon Collar"};
 
 --Hide Name
 local a = coroutine.wrap(function()
@@ -72,17 +71,19 @@ end
 
 pcall(function()
     while wait() do
-        if _G.autofarm and lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") and lp.Health.Value ~= 0 then
+        if _G.autofarm and lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") then
 			--EXPERIMENT WITH DISTANCE FOR GREEN DEMON
 			if _G.mob == "Green Demon" then _G.distFromMob = 10; else _G.distFromMob = 6; end
 			if _G.mob == "Zenitsu" or _G.mob == "Green Demon" then
 				local dist = (spawns[_G.mob].Position - lp.Character.HumanoidRootPart.Position).magnitude;
-				if dist <= 100 then
-					lp.Character.HumanoidRootPart.CFrame = spawns[_G.mob];
-				else
-					tpBypass.bindable:Invoke();
-					lp.Character.HumanoidRootPart.CFrame = spawns[_G.mob];
-				end
+				local t = dist / _G.speed;
+				
+				local tweenInfo = TweenInfo.new(t, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0);
+				local tween = ts:Create(lp.Character.HumanoidRootPart, tweenInfo, {CFrame = spawns[_G.mob]});
+				tween:Play();
+				repeat wait() until (spawns[_G.mob].Position - lp.Character.HumanoidRootPart.Position).magnitude <= 100;
+				tween:Cancel();
+				lp.Character.HumanoidRootPart.CFrame = spawns[_G.mob];
 			end
 		
             local closest = getClosestMob();
@@ -92,53 +93,58 @@ pcall(function()
             
 			local dist = (closest.HumanoidRootPart.Position - lp.Character.HumanoidRootPart.Position).magnitude;
             local t = dist / _G.speed;
-			
-			tpBypass.bindable:Invoke();
-			lp.Character.HumanoidRootPart.CFrame = CFrame.new((closest.HumanoidRootPart.Position + Vector3.new(0, _G.distFromMob, 0)), closest.HumanoidRootPart.Position);
-			
-			_G.notExecuted = true;
-			
-			repeat wait()
-				lp.Character.HumanoidRootPart.CFrame = CFrame.new((closest.HumanoidRootPart.Position + Vector3.new(0, _G.distFromMob, 0)), closest.HumanoidRootPart.Position);
-				if closest:FindFirstChild("Block") and not closest:FindFirstChild("Ragdoll") then
-					if lp.Stamina.Value >= 20 then
-						rs.Remotes.Async:FireServer(style, "Heavy");
+            
+            if dist <= _G.maxDist then
+				local tweenInfo = TweenInfo.new(t, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0);
+                local tween = ts:Create(lp.Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new((closest.HumanoidRootPart.Position + Vector3.new(0, _G.distFromMob, 0)), closest.HumanoidRootPart.Position)});
+                tween:Play();
+                repeat wait() until (closest.HumanoidRootPart.Position - lp.Character.HumanoidRootPart.Position).magnitude <= 100;
+                tween:Cancel();
+    
+                _G.notExecuted = true;
+                
+                repeat wait()
+                    lp.Character.HumanoidRootPart.CFrame = CFrame.new((closest.HumanoidRootPart.Position + Vector3.new(0, _G.distFromMob, 0)), closest.HumanoidRootPart.Position);
+					if closest:FindFirstChild("Block") and not closest:FindFirstChild("Ragdoll") then
+						if lp.Stamina.Value >= 20 then
+							rs.Remotes.Async:FireServer(style, "Heavy");
+						end
 					end
-				end
-				
-				if not closest:FindFirstChild("Ragdoll") and not closest:FindFirstChild("Block") then wait(0.45); rs.Remotes.Async:FireServer(style, "Server"); end
-				
-				if closest:FindFirstChild("Down") then
-					local count = 0;
-					repeat wait()
-						lp.Character.HumanoidRootPart.CFrame = closest.HumanoidRootPart.CFrame;
-						wait(0.1);
-						count = count + 1;
-						vim:SendKeyEvent(true, Enum.KeyCode.B, false, game);
-						wait(0.5);
-						vim:SendKeyEvent(false, Enum.KeyCode.B, false, game);
-						if lp.Character:FindFirstChild("OnExecute") or closest:FindFirstChild("Executing") then
-							local cf = lp.Character.HumanoidRootPart.CFrame;
-							local count2 = 0;
-							repeat wait() lp.Character.HumanoidRootPart.Anchored = false; lp.Character.HumanoidRootPart.CFrame = cf * CFrame.new(0, -15, 0); count2 = count2 + 1; until closest:FindFirstChild("Executed") or count2 >= 100;	
-						end
-					until closest:FindFirstChild("Executed") or count > 10;
-					local br = ws.ChildAdded:Connect(function(c)
-						c:WaitForChild("ItemName");
-						if c.Name == "DropItem" and drops[c.Name] then
-							rs.Remotes.Async:FireServer("Character", "Interaction", c);
-							vim:SendKeyEvent(true, Enum.KeyCode.E, false, game);
+					
+					if not closest:FindFirstChild("Ragdoll") and not closest:FindFirstChild("Block") then wait(0.45); rs.Remotes.Async:FireServer(style, "Server"); end
+					
+                    if closest:FindFirstChild("Down") then
+                        local count = 0;
+                        repeat wait()
+                            lp.Character.HumanoidRootPart.CFrame = closest.HumanoidRootPart.CFrame;
+							wait(0.1);
+							count = count + 1;
+							vim:SendKeyEvent(true, Enum.KeyCode.B, false, game);
 							wait(0.5);
-							vim:SendKeyEvent(false, Enum.KeyCode.E, false, game);
-						end
-					end);
-					wait(0.7);
-					_G.notExecuted = false;
-					br:Disconnect();
-				end
-			until not _G.autofarm or not _G.notExecuted
-			_G.noclip = true;
-			wait(1.5);
+							vim:SendKeyEvent(false, Enum.KeyCode.B, false, game);
+							if lp.Character:FindFirstChild("OnExecute") or closest:FindFirstChild("Executing") then
+								local cf = lp.Character.HumanoidRootPart.CFrame;
+								local count2 = 0;
+								repeat wait() lp.Character.HumanoidRootPart.Anchored = false; lp.Character.HumanoidRootPart.CFrame = cf * CFrame.new(0, -15, 0); count2 = count2 + 1; until closest:FindFirstChild("Executed") or count2 >= 100;	
+							end
+                        until closest:FindFirstChild("Executed") or count > 10;
+                        local br = ws.ChildAdded:Connect(function(c)
+                            c:WaitForChild("ItemName");
+                            if c.Name == "DropItem" and drops[c.Name] then
+                                rs.Remotes.Async:FireServer("Character", "Interaction", c);
+                                vim:SendKeyEvent(true, Enum.KeyCode.E, false, game);
+                                wait(0.5);
+                                vim:SendKeyEvent(false, Enum.KeyCode.E, false, game);
+                            end
+                        end);
+                        wait(0.7);
+                        _G.notExecuted = false;
+                        br:Disconnect();
+                    end
+                until not _G.autofarm or not _G.notExecuted
+                _G.noclip = true;
+                wait(1.5);
+            end
         end
     end
 end)
