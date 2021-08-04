e87371784018faf6b3c1dc25f7f60596f24cdd5d
french-spy <50294457+french-spy@users.noreply.github.com>
2021-08-04 14:43:33 +0300

    Update pls god.lua

--- pls god.lua
+++ pls god.lua
@@ -10,7 +10,9 @@ _G.notExecuted = true; --dont touch
 		repeat wait() until game:IsLoaded();
 		wait(5.5);
 		print("GameLoaded");
-		game:WaitForChild("Players"):WaitForChild("LocalPlayer"):WaitForChild("PlayerGui"):WaitForChild("LoadingScreen");
+		game:WaitForChild("Players");
+		repeat wait() until game.Players.LocalPlayer;
+		game.Players.LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("LoadingScreen");
 		
 		if #game.Players:GetChildren() == 1 then
 			local lp = game:service"Players".LocalPlayer;
