c5d3ddc90fcfd702ca58301e654d62da6e1e20ab
french-spy <50294457+french-spy@users.noreply.github.com>
2021-09-05 22:35:13 +0300

    Update aut stand invis.lua

--- aut stand invis.lua
+++ aut stand invis.lua
@@ -1,7 +1,8 @@
 local lp = game:service"Players".LocalPlayer;
+getgenv().stand_invis = true;
 coroutine.wrap(function()
     while wait() do
-        if lp.Character and lp.Character:FindFirstChild("Stand") then
+        if stand_invis and lp.Character and lp.Character:FindFirstChild("Stand") then
             for i,v in pairs(lp.Character.Stand:GetChildren()) do
                 if v.Name:find("HumanoidRootPart") and v:IsA("Part") and v:FindFirstChild("RootJoint") then
                     v.RootJoint:Destroy();
