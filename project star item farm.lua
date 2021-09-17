8f434c9707b8019c6cc84e6bf21f17660b0b1946
french-spy <50294457+french-spy@users.noreply.github.com>
2021-09-17 17:09:53 +0300

    Update project star item farm.lua

--- project star item farm.lua
+++ project star item farm.lua
@@ -2,17 +2,17 @@ local lp = game:service"Players".LocalPlayer;
 local rs = game:service"ReplicatedStorage";
 local ws = workspace;
 local run_s = game:service"RunService";
+
 getgenv().item_farm = true;
 getgenv().hide_character = true;
-coroutine.wrap(function()
-    run_s.RenderStepped:Connect(function()
-        if item_farm and lp.Character and lp.Character:FindFirstChild("Humanoid") then
-            lp.Character.Humanoid:ChangeState(11);
-        end
-    end)
-end)();
+
 local instance_names = {"Torso", "Head", "Right Arm", "Left Arm", "Left Leg", "Right Leg"};
 coroutine.wrap(function()
+	run_s.RenderStepped:Connect(function()
+		if item_farm and lp.Character and lp.Character:FindFirstChild("Humanoid") then
+			lp.Character.Humanoid:ChangeState(11);
+		end
+    end);
     while wait() do
         if hide_character and lp and lp.Character then
             for i,v in pairs(lp.Character:GetChildren()) do
@@ -27,12 +27,16 @@ coroutine.wrap(function()
     while wait() do
         if item_farm and lp.Character and lp.Character.PrimaryPart then
             for i,v in pairs(ws.Drops.Active:GetChildren()) do
-                if v:FindFirstChildOfClass("ProximityPrompt", true) and v:FindFirstChild("DropCenter") then
-                    lp.Character.PrimaryPart.CFrame = v.DropCenter.CFrame * CFrame.new(0, -5, 0);
-                    wait(.25);
-                    fireproximityprompt(v:FindFirstChildOfClass("ProximityPrompt", true), 10);
-                    wait(.25);
-                end
+                if not table.find(item_blacklist, v.Name) and v:FindFirstChildOfClass("ProximityPrompt", true) then
+					local a = v:FindFirstChildOfClass("ProximityPrompt", true);
+					local b = a:FindFirstAncestorOfClass("Part") or a:FindFirstAncestorOfClass("MeshPart") or a:FindFirstAncestorOfClass("Handle");
+					if b and a then
+						lp.Character.PrimaryPart.CFrame = b.CFrame * CFrame.new(0, -5, 0);
+						wait(.25);
+						fireproximityprompt(v:FindFirstChildOfClass("ProximityPrompt", true), 10);
+						wait(.25);
+					end
+				end
             end
         end
     end
