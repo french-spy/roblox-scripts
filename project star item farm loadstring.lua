ba0d478bba96b0e5be80a7863ca870a44f53c794
french-spy <50294457+french-spy@users.noreply.github.com>
2021-09-17 17:12:58 +0300

    Create project star item farm loadstring.lua

---
+++ project star item farm loadstring.lua
@@ -0,0 +1,5 @@
+getgenv().hide_character = true; --Recommended to keep true
+getgenv().item_farm = true; --set to false if u want to disable it
+getgenv().item_blacklist = {"Banana"}; --Add items you dont want to pickup CASE AND SPACE SENSITIVE
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/french-spy/roblox-scripts/main/project%20star%20item%20farm.lua"))();
