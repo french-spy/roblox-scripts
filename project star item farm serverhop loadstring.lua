e8e2dbda897eb989e68c741cc090a85fe4dbb075
french-spy <50294457+french-spy@users.noreply.github.com>
2021-09-17 17:15:43 +0300

    Create project star item farm serverhop loadstring.lua

---
+++ project star item farm serverhop loadstring.lua
@@ -0,0 +1,4 @@
+getgenv().hide_character = true; --Recommended to keep true
+getgenv().item_blacklist = {"Banana", "Cabbage"}; --Add items you dont want to pickup CASE AND SPACE SENSITIVE
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/french-spy/roblox-scripts/main/project%20star%20item%20farm%20serverhop.lua"))();
