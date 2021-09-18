d7ddd15fc193e3be54b1ba713d19b7ad89ae3362
french-spy <50294457+french-spy@users.noreply.github.com>
2021-09-18 13:24:04 +0300

    Update project star item farm serverhop loadstring.lua

--- project star item farm serverhop loadstring.lua
+++ project star item farm serverhop loadstring.lua
@@ -1,6 +1,4 @@
 getgenv().hide_character = true;
 getgenv().item_blacklist = {"Banana", "Orebag", "Cactus Juice", "Tire", "Cabbage"};
-getgenv().speed = 85; --Recommended speed, lower it if u get tped back
-getgenv().dist_before_tp = 200; --Lower it if u get tped back
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/french-spy/roblox-scripts/main/project%20star%20item%20farm%20serverhop.lua"))();
