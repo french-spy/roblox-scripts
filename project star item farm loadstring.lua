7ac437c6cd3155a755c99d275dd60415a37eff1c
french-spy <50294457+french-spy@users.noreply.github.com>
2021-09-18 11:12:47 +0300

    Update project star item farm loadstring.lua

--- project star item farm loadstring.lua
+++ project star item farm loadstring.lua
@@ -1,5 +1,7 @@
-getgenv().hide_character = true; --Recommended to keep true
-getgenv().item_farm = true; --set to false if u want to disable it
-getgenv().item_blacklist = {"Banana"}; --Add items you dont want to pickup CASE AND SPACE SENSITIVE
+getgenv().hide_character = true;
+getgenv().item_blacklist = {"Banana", "Orebag", "Cactus Juice", "Tire", "Cabbage"};
+getgenv().speed = 70; --Recommended speed, lower it if u get tped back
+getgenv().dist_before_tp = 100; --Lower it if u get tped back
+getgenv().item_farm = true;
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/french-spy/roblox-scripts/main/project%20star%20item%20farm.lua"))();
