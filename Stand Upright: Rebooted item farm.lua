616e3459093c1829c7b279b8d7f9a92fa1021fad
french-spy <50294457+french-spy@users.noreply.github.com>
2022-05-08 22:01:50 +0300

    Update Stand Upright: Rebooted item farm.lua

--- Stand Upright: Rebooted item farm.lua
+++ Stand Upright: Rebooted item farm.lua
@@ -9,9 +9,12 @@ local lp = game:service"Players".LocalPlayer;
 local ws = workspace;
 local ts = game:service"TweenService";
 
-if lp.PlayerGui.MenuGUI.Enabled then
-    firesignal(lp.PlayerGui.MenuGUI.Play.MouseButton1Click);
-end 
+while task.wait() do
+    if lp.PlayerGui.MenuGUI.Enabled then
+        firesignal(lp.PlayerGui.MenuGUI.Play.MouseButton1Click);
+        break;
+    end 
+end
 task.wait(1);
 
 local PlaceID = game.PlaceId
