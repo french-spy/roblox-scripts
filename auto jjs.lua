f758089ce2b7dfb719ac112147dfbd44dfc130d1
french-spy <50294457+french-spy@users.noreply.github.com>
2022-05-31 23:06:26 +0300

    Update auto jjs.lua

--- auto jjs.lua
+++ auto jjs.lua
@@ -1,4 +1,5 @@
-local jjs = 60;
+local choice = 11;
+local b = "HJ";
 
 local a = {
     "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE",
@@ -19,9 +20,36 @@ local a = {
 local say = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest;
 local vim = game:service"VirtualInputManager";
 for i=1, choice do
-    say:FireServer(a[i] .. "!", "All");
-    vim:SendKeyEvent(true, Enum.KeyCode.Space, false, game);
-    task.wait();
-    vim:SendKeyEvent(false, Enum.KeyCode.Space, false, game);
-    task.wait(2.5);
+    if b == "JJ" then
+        say:FireServer(a[i] .. "!", "All");
+        task.wait();
+        vim:SendKeyEvent(true, Enum.KeyCode.Space, false, game);
+        task.wait();
+        vim:SendKeyEvent(false, Enum.KeyCode.Space, false, game);
+        task.wait(3);
+    elseif b == "HJ" then
+        for i2=1, #a[i] do
+            if i2 == #a[i] then
+                say:FireServer(a[i]:sub(i2, i2), "All");
+                task.wait();
+                vim:SendKeyEvent(true, Enum.KeyCode.Space, false, game);
+                task.wait();
+                vim:SendKeyEvent(false, Enum.KeyCode.Space, false, game);
+                task.wait(3);
+                say:FireServer(a[i] .. "!", "All");
+                task.wait();
+                vim:SendKeyEvent(true, Enum.KeyCode.Space, false, game);
+                task.wait();
+                vim:SendKeyEvent(false, Enum.KeyCode.Space, false, game);
+                task.wait(3);
+            else
+                say:FireServer(a[i]:sub(i2, i2), "All");
+                task.wait();
+                vim:SendKeyEvent(true, Enum.KeyCode.Space, false, game);
+                task.wait();
+                vim:SendKeyEvent(false, Enum.KeyCode.Space, false, game);
+                task.wait(2.5);
+            end
+        end
+    end
 end
