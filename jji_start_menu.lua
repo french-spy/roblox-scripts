b0ae43a1f96e54dfc967e0d8d77352722e581a19
french-spy <50294457+french-spy@users.noreply.github.com>
2025-01-04 13:53:01 +0200

    Update jji_start_menu.lua

--- jji_start_menu.lua
+++ jji_start_menu.lua
@@ -4,12 +4,13 @@ local lp = game:service"Players".LocalPlayer
 while not lp:FindFirstChild("PlayerGui") do task.wait() end
 while not lp.PlayerGui:FindFirstChild("Menu") do task.wait() end
 while not lp.PlayerGui.Menu:FindFirstChild("MenuButtons") do task.wait() end
+while not lp.PlayerGui.Menu.MenuButtons.Visible do task.wait() end
 
 pcall(function()
     repeat task.wait()
         for i,v in getconnections(lp.PlayerGui.Menu.MenuButtons.Play.MouseButton1Click) do
             v.Function()
         end
-    until not lp.PlayerGui.Menu.Enabled
+    until not lp.PlayerGui.Menu.MenuButtons.Visible
 end)
     
