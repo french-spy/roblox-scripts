d90643abc78ee9df75240709cae115b39575d5c5
french-spy <50294457+french-spy@users.noreply.github.com>
2025-01-04 13:45:57 +0200

    Update jji_start_menu.lua

--- jji_start_menu.lua
+++ jji_start_menu.lua
@@ -1,7 +1,9 @@
 while not game do task.wait() end
 while not game:service"Players".LocalPlayer do task.wait() end
 local lp = game:service"Players".LocalPlayer
-while not lp:FindFirstChild("PlayerGui") and not lp.PlayerGui:FindFirstChild("Menu") do task.wait() end
+while not lp:FindFirstChild("PlayerGui") do task.wait() end
+while not lp.PlayerGui:FindFirstChild("Menu") do task.wait() end
+while not lp.PlayerGui.Menu:FindFirstChild("MenuButtons") do task.wait()
 
 for i,v in getconnections(lp.PlayerGui.Menu.MenuButtons.Play.MouseButton1Click) do
     v.Function()
